740f27b29c5f819f189f7b0df412f53ec9cdb3e9
Sogon-Njugush <sogon5045@gmail.com>
2024-07-04 15:13:07 +0300

    clerk confirgurations and user login

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,29 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image"
 
 const AddPost =() => {
+// // get user id from clerk
+// const {userId} = auth();
+// // console.log(userId)
 
+// if(!userId) return;
+
+// const testAction = async(formData:FormData) =>{
+//   "use server"
+//   const desc = formData.get("desc") as string;
+//   try {
+//    const res = await prisma.post.create({
+//       data:{
+//         userId:userId,
+//         desc:desc,
+//       },
+//     });
+//     console.log(res)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
@@ -10,10 +32,11 @@ const AddPost =() => {
          {/* POST */}
          <div className="flex-1">
           {/* TEXT INPUT */}
-        <div className="flex gap-4">
-            <textarea placeholder="whats on your mind?" className="flex-1 bg-slate-100 rounded-lg p-2"></textarea>
+        <form action="" className="flex gap-4">
+            <textarea placeholder="whats on your mind?" name="desc" className="flex-1 bg-slate-100 rounded-lg p-2"></textarea>
             <Image src="/emoji.png" width={20} height={20} alt="" className="w-5 h-5 cursor-pointer self-end"/>
-        </div>
+            <button>Send</button>
+        </form>
          {/* POST OPTIONS */}
          <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
             <div className="flex items-center gap-2 cursor-pointer">
